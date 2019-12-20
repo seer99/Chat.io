feb2e5598d15b06588d40034f121f6dcf15c1dc5
sachithamh <5784803+sachithamh@users.noreply.github.com>
2019-12-20 11:32:25 +0530

    Update setup Main functions

--- setup.js
+++ setup.js
@@ -7,8 +7,8 @@ var DATABASE_NAME = serviceAccount.project_id //for [DEFAULT]  if you want to co
 //<DATABASE_NAME>
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${DATABASE_NAME}.firebaseio.com`
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${DATABASE_NAME}.firebaseio.com`
 });
 
 //Data Base firestore create reference
@@ -17,41 +17,26 @@ var firestore = admin.firestore()
 
 console.log("Create Collection with Sample Data...")
 
-//user
-console.log("Create user...")
-let userRef = firestore.collection('user');
-userRef.add({displayName:"sachithamh"}).then((reference)=>{//othe data you can add if you need to keep more data
-    console.log("Create user sucessfully...")
-}).catch((error)=>{
-    console.error(error);
-});
+//user collection create
+function createUser(userId,data) {
 
-//chat group
-console.log("Create chat_group...")
-let userRef = firestore.collection('chat_group');
-userRef.add({displayName:"sachithamh"}).then((reference)=>{//othe data you can add if you need to keep more data
-    console.log("Create user sucessfully...")
-}).catch((error)=>{
-    console.error(error);
-});
+}
 
-//subscribe
-console.log("Create user...")
-let userRef = firestore.collection('user');
-userRef.add({displayName:"sachithamh"}).then((reference)=>{//othe data you can add if you need to keep more data
-    console.log("Create user sucessfully...")
-}).catch((error)=>{
-    console.error(error);
-});
+//chat group collection create
+function createChatGroup(userId,chatGroupName) {
+}
 
-//chat
-console.log("Create user...")
-let userRef = firestore.collection('user');
-userRef.add({displayName:"sachithamh"}).then((reference)=>{//othe data you can add if you need to keep more data
-    console.log("Create user sucessfully...")
-}).catch((error)=>{
-    console.error(error);
-});
+
+//subscribe collection create
+function createSubscribe() {
+}
+
+//chat collection create
+function createChat() {
+
+    
+}
+
 
 
 
