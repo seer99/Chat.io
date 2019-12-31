e00a10b947ad8aeb59bb1b6d80e94ce6fd8321d1
Sachitha <sachithamh@gmail.com>
2019-12-31 07:06:25 +0530

    Update setup.js
    update

--- setup.js
+++ setup.js
@@ -20,14 +20,7 @@ console.log("Create Collection with Sample Data...")
 //user collection create
 function createUser(userId,data) {
 
-//chat group
-console.log("Create chat_group...")
-let userRef = firestore.collection('chat_group');
-userRef.add({user_id:"sachithamh"}).then((reference)=>{//othe data you can add if you need to keep more data
-    console.log("Create user sucessfully...")
-}).catch((error)=>{
-    console.error(error);
-});
+}
 
 //chat group collection create
 function createChatGroup(userId,chatGroupName) {
