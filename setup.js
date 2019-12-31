4c5af40b275580fd9ab9d450cbc468172d90a2be
Sachitha <sachithamh@gmail.com>
2019-12-31 07:39:56 +0530

    Update setup.js
    add create use sucess fully

--- setup.js
+++ setup.js
@@ -16,19 +16,31 @@ var firestore = admin.firestore()
 
 
 console.log("Create Collection with Sample Data...")
+/*create sample users*/
+createUser("userid_1",{"name":"USER_1"})
+createUser("userid_2",{"name":"USER_2"})
 
 //user collection create
 function createUser(userId,data) {
-
+    firestore.collection('users').doc(userId).set(data).then(ref => {
+        console.log(ref)
+        console.log("create user id -> " + userId); 
+    }).catch(error => {
+        console.log(error);
+    })
+        
 }
 
 //chat group collection create
 function createChatGroup(userId,chatGroupName) {
+
+
 }
 
 
 //subscribe collection create
 function createSubscribe() {
+
 }
 
 //chat collection create
